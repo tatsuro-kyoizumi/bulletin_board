910f61eef94f48b0a5c33072939b9c623f71b3dd
tatsuro-kyoizumi <tatsu.sd.us@gmail.com>
2020-03-27 13:08:08 +0000

    Initial Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:description)
+  end
+end
